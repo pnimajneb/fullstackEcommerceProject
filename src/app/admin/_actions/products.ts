5b2cce57b400b5c8669b73b0f8357cd4a2640930
benjaminp <bpeters@posteo.de>
2024-06-17 12:11:44 +0200

    implementing zod schema and saving files and images in products.ts

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -1,7 +1,53 @@
 "use server"
 // this file refers to the form in the ProductForm.tsx for which we need actions. in the _actions folder we insert different .ts files that point to different components
 
+import db from "@/db/db"
+import { z } from "zod"
+import fs from "fs/promises"
+import { redirect } from "next/navigation"
+
+//in zod schema there is not a handy way such as .file which is why we need to create our own schema:
+// we need to choose File because the file is an instance of File, if it's not we say Required
+const fileSchema = z.instanceof(File, {message: "Required"})
+const imageSchema = fileSchema.refine(file => file.size === 0 || file.type.startsWith("image/"))
+
+const addSchema = z.object({
+    name: z.string().min(1),
+    description: z.string().min(1),
+    //formData is using string values which is why we need to coerce it to number
+    priceInCents: z.coerce.number().int().min(1),
+    file: fileSchema.refine(file => file.size > 0, "Required"),
+    image: imageSchema.refine(file => file.size > 0, "Required")
+})
+
 // actions must be async
 export async function addProduct(formData: FormData) {
-    console.log(formData)
+    const result = addSchema.safeParse(Object.fromEntries(formData.entries()))
+    if(result.success === false) {
+        return result.error.formErrors.fieldErrors
+    }
+
+    const data = result.data
+
+    // create the path where we want the file to save it to:
+    await fs.mkdir("products", { recursive:true })
+    const filePath = `products/${crypto.randomUUID()}-${data.file.name}`
+    // add/save the file to the actual file path:
+    await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()))
+    // --> it converts the file into a Buffer and we can pass the Buffer along to writeFile - essentially we are just taking our file from the state its currently in and converting it into a file that node.js knows how to use
+
+    await fs.mkdir("public/products", { recursive:true })
+    const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
+    await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
+
+    db.product.create({data: {
+        name: data.name,
+        description: data.description,
+        priceInCents: data.priceInCents,
+        //first safe the file to the file system before we actually can safe the path to them inside of the database which we do with await fs. ... further up
+        filePath,
+        imagePath
+    }})
+
+    redirect("/admin/products")
 }
--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -14,38 +14,41 @@ export function ProductForm() {
   return (
     <>
       {/* the action label uses a server action that is shown in _actions */}
-      <form action={addProduct} className="space-y-8"></form>
-      <div className="space-y-2">
-        <Label htmlFor="name">Name</Label>
-        <Input type="text" id="name" name="name" required />
-      </div>
-      <div className="space-y-2">
-        <Label htmlFor="priceInCents">Price In Cents</Label>
-        <Input
-          type="number"
-          id="priceInCents"
-          name="priceInCents"
-          required
-          value={priceInCents}
-          onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
-        />
-        <div className="text-muted-foreground">
-          {formatCurrency((priceInCents || 0) / 100)}
+      <form action={addProduct} className="space-y-8">
+        <div className="space-y-2">
+          <Label htmlFor="name">Name</Label>
+          <Input type="text" id="name" name="name" required />
         </div>
         <div className="space-y-2">
-          <Label htmlFor="description">Description</Label>
-          <Textarea id="description" name="description" required />
+          <Label htmlFor="priceInCents">Price In Cents</Label>
+          <Input
+            type="number"
+            id="priceInCents"
+            name="priceInCents"
+            required
+            value={priceInCents}
+            onChange={(e) =>
+              setPriceInCents(Number(e.target.value) || undefined)
+            }
+          />
+          <div className="text-muted-foreground">
+            {formatCurrency((priceInCents || 0) / 100)}
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="description">Description</Label>
+            <Textarea id="description" name="description" required />
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="file">Name</Label>
+            <Input type="file" id="file" name="file" required />
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="image">Image</Label>
+            <Input type="file" id="image" name="image" required />
+          </div>
+          <Button type="submit">Save</Button>
         </div>
-        <div className="space-y-2">
-          <Label htmlFor="file">Name</Label>
-          <Input type="file" id="file" name="file" required />
-        </div>
-        <div className="space-y-2">
-          <Label htmlFor="image">Image</Label>
-          <Input type="file" id="image" name="image" required />
-        </div>
-        <Button type="submit">Save</Button>
-      </div>
+      </form>
     </>
   );
 }
