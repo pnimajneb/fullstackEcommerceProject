fab74b456d40afb70b3ba3807960466ff809bdfe
benjaminp <bpeters@posteo.de>
2024-06-27 13:38:15 +0200

    implementing customerFacing withpage.tsx and ProductCard.tsx for users that purchased products

---
+++ src/app/(customerFacing)/layout.tsx
@@ -0,0 +1,21 @@
+import { Nav, NavLink } from "@/components/Nav";
+
+// //This removes caching for all the admin pages
+// export const dynamic = "force-dynamic";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Nav>
+        <NavLink href="/admin">Home</NavLink>
+        <NavLink href="/products">Products</NavLink>
+        <NavLink href="/orders">My Orders</NavLink>
+      </Nav>
+      <div className="container my-6">{children}</div>
+    </>
+  );
+}
