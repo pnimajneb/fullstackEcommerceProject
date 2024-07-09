b3eb9fbf4e45d71e09f900b73875003bb378c626
benjaminp <bpeters@posteo.de>
2024-07-09 11:27:27 +0200

    creating a new route.ts in download folder to make the user able to download the purchased product

--- src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -50,7 +50,13 @@ export default async function SuccessPage({
           </div>
           <Button className="mt-4" size="lg" asChild>
             {isSuccess ? (
-              <a></a>
+              <a
+                href={`/products/download/${await createDownloadVerification(
+                  product.id
+                )}`}
+              >
+                Download
+              </a>
             ) : (
               <Link href={`/products/${product.id}/purchase`}>Try Again</Link>
             )}
@@ -59,4 +65,16 @@ export default async function SuccessPage({
       </div>
     </div>
   );
+}
+
+async function createDownloadVerification(productId: string) {
+  return (
+    await db.downloadVerification.create({
+      data: {
+        productId,
+        //one day, therefore 24hours, in milliseconds
+        expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
+      },
+    })
+  ).id;
 }
